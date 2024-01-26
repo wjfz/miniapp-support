dbe350a9ccfa9afc816f28fab165a8e0b7e45409
zxy <2544682494@qq.com>
2024-01-26 18:30:52 +0800

    Upgrade gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,7 +73,7 @@
 
 fun properties(key: String) = project.findProperty(key).toString()
 plugins {
-    id("org.jetbrains.intellij") version "1.16.1"
+    id("org.jetbrains.intellij") version "1.17.0"
     java
     id("org.jetbrains.kotlin.jvm") version "1.9.22"
 }
